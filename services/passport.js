a037373f964f0dd5d43a4ad1e9176b6aa04dc962
johnson <phantruongson96@gmail.com>
2021-03-06 01:48:02 -0500

    add client building side app

--- services/passport.js
+++ services/passport.js
@@ -22,19 +22,17 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-  }, (accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id })
-      .then((existingUser) => {
-        if(existingUser) {
-          // we already have a record with the given profile id
-          done(null, existingUser);
-        }else {
-          // we don't have record with this ID, make a new record
-          new User({ googleId: profile.id})
-            .save()
-            .then(user => done(null, user));
-        }
-      })
+  }, 
+  async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ googleId: profile.id });
+
+    if(existingUser) {
+      // we already have a record with the given profile id
+      return done(null, existingUser);
+    }
     
+    // we don't have record with this ID, make a new record
+    const user = await new User({ googleId: profile.id}).save();
+    done(null, user);
   })
 );
